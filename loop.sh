299e6d45e3aed09d8dcdc9a2ab40d71510112fad
Mathieu GOULIN <mathieu.Goulin@gadz.org>
2018-08-04 22:37:28 +0200

    add wind data

--- loop.sh
+++ loop.sh
@@ -10,15 +10,20 @@ do
     humidity=$( cat $tmp | jq .main.humidity )
     temp_min=$( cat $tmp | jq .main.temp_min )
     temp_max=$( cat $tmp | jq .main.temp_max ) 
+    wind_speed=$( cat $tmp | jq .wind.speed )
+    wind_direction=$( cat $tmp | jq .wind.deg )
     echo "********************************************************************"
     echo " Ville : $city"
     echo " temperature: $temp"
     echo " humidity: $humidity"
     echo " temp_min: $temp_min"
     echo " temp_max: $temp_max"
+    echo " wind_speed: $wind_speed"
+    echo " wind_direction: $wind_direction"
+    echo " pressure: $pressure"
     echo ""
 
-    curl -k -i -XPOST "$INFLUX_URL/write?db=$INFLUX_DATABASE&u=$INFLUX_USER&p=$INFLUX_PASSWORD" --data-binary "weather,station=$city temperature_K=$temp,humidity=$humidity,temp_min=$temp_min,temp_max=$temp_max,pressure=$pressure"
+    curl -k -i -XPOST "$INFLUX_URL/write?db=$INFLUX_DATABASE&u=$INFLUX_USER&p=$INFLUX_PASSWORD" --data-binary "weather,station=$city temperature_K=$temp,humidity=$humidity,temp_min=$temp_min,temp_max=$temp_max,wind_speed=$wind_speed,wind_direction=$wind_direction,pressure=$pressure"
 
     
     sleep 60
